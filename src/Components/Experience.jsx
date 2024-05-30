a3e19beff1ef1757a961c5679f529d4233e27f20
suyash159 <harshlodwal525@gmail.com>
2024-05-30 23:58:22 +0530

    modified_experience_page

--- src/Components/Experience.jsx
+++ src/Components/Experience.jsx
@@ -7,11 +7,11 @@ function Experience() {
     <>
       <Cursor />
       <div className="mt-[5%] w-[50%] m-auto">
-        {/* <h1 className='font-bold text-5xl mb-2
+        <h1 className='font-bold text-5xl mb-2
                     bg-gradient-to-br  from-yellow-300 from-30% to-purple-500 to-90%
                     inline-block text-transparent bg-clip-text pb-3
                     '>Experience that I have gained
-        </h1> */}
+        </h1>
         <h1 className='font-bold text-5xl mb-5
                     bg-gradient-to-br  from-yellow-300 from-30% to-purple-500 to-90%
                     inline-block text-transparent bg-clip-text pb-5'
